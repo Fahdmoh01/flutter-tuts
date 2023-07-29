ce0996ad2a0f321ffb52537bfc34a35fac12ab2a
Fahd Mohammed <fahdmoh.1@gmail.com>
2023-07-29 12:30:22 +0100

    TextField chapter completed

--- currency_converter/lib/currency_converter_material_page.dart
+++ currency_converter/lib/currency_converter_material_page.dart
@@ -6,8 +6,43 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Colors.blueGrey,
       body: Center(
-        child: Text('Hello World!'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '0',
+              style: TextStyle(
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 255, 255, 255),
+              ),
+            ),
+            TextField(
+              style: TextStyle(
+                color: Colors.black,
+              ),
+              decoration: InputDecoration(
+                hintText: 'Please enter the amount in USD',
+                hintStyle: TextStyle(color: Colors.black),
+                prefixIcon: Icon(Icons.monetization_on),
+                prefixIconColor: Colors.black,
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.black,
+                    width: 2.0,
+                    style: BorderStyle.solid,
+                    strokeAlign: BorderSide.strokeAlignOutside,
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
